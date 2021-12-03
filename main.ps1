a67d28832aa365af4e2b9c8781db352b4bc83431
Aidan Brown <45271332+h4wk590@users.noreply.github.com>
2021-12-02 16:14:51 -0800

    Switch from Winget to Chocolatey

--- main.ps1
+++ main.ps1
@@ -1,68 +1,22 @@
-#Install WinGet
+# Needs to be run as Admin
 
-$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
-if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
-    "Installing winget Dependencies"
-    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+#requires -RunAsAdministrator
 
-    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+# Set the Execution policy
+Set-ExecutionPolicy -Unrestricred
 
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $releases = Invoke-RestMethod -uri $releases_url
-    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+# Install Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-    "Installing winget from $($latestRelease.browser_download_url)"
-    Add-AppxPackage -Path $latestRelease.browser_download_url
-}
-else {
-    "winget already installed"
-}
+# Install apps (Chocolatey won't install it if it's already on the system)
 
-#Configure WinGet
-Write-Output "Configuring winget"
+choco install firefox -fy
+choco install googlechrome -fy
+choco install steam-client -fy
+choco install discord -fy
+choco install notepadplusplus -fy
+choco install vscode -fy
+choco install spotify -fy
+choco install greenshot -fy
+choco install powertoys -fy
 
-#winget config path from: https://github.com/microsoft/winget-cli/blob/master/doc/Settings.md#file-location
-$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json";
-$settingsJson = 
-@"
-    {
-        // For documentation on these settings, see: https://aka.ms/winget-settings
-        "experimentalFeatures": {
-          "experimentalMSStore": true,
-        }
-    }
-"@;
-$settingsJson | Out-File $settingsPath -Encoding utf8
-
-#Install New apps
-Write-Output "Installing Apps"
-$apps = @(
-    @{name = "Google.Chrome" }, 
-    @{name = "Microsoft.PowerShell" }, 
-    @{name = "Microsoft.VisualStudioCode" },  
-    @{name = "Mozilla.Firefox" }, 
-    @{name = "Microsoft.PowerToys" }, 
-    @{name = "Runelite.Runelite" }, 
-    @{name = "Valve.Steam" },
-    @{name = "Discord.Discord" },
-    @{name = "Blizzard.BattleNet" },
-    @{name = "Notepad++.Notepad++" },
-    @{name = "Spotify.Spotify" },
-    @{name = "RiotGames.LeagueOfLegends" },
-    @{name = "VideoLAN.VLC" }
-);
-Foreach ($app in $apps) {
-    $listApp = winget list --exact -q $app.name
-    if (![String]::Join("", $listApp).Contains($app.name)) {
-        Write-host "Installing:" $app.name
-        if ($app.source -ne $null) {
-            winget install --exact --silent $app.name --source $app.source
-        }
-        else {
-            winget install --exact --silent $app.name 
-        }
-    }
-    else {
-        Write-host "Skipping Install of " $app.name
-    }
-}
