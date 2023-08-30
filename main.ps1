2823997b6016b6ff065eec52250ddc31763844d3
h4wk590 <aidanbrownj@gmail.com>
2023-08-29 17:18:14 -0700

    Add functions and wsl2 installation

--- main.ps1
+++ main.ps1
@@ -9,16 +9,44 @@ Windows Install script w/ bloat removal
 # Set the Execution policy
 Set-ExecutionPolicy Unrestricted
 
+function wslInstall {
+# Check if the script is running with administrative privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as an administrator."
+    Exit
+}
+
+# Enable Virtual Machine Platform and Hyper-V features required for WSL 2
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+
+# Download and install the WSL 2 Linux kernel update package
+$wslUpdateUrl = "https://aka.ms/wsl2kernel"
+$wslUpdatePackagePath = "$env:TEMP\wsl_update.msi"
+Invoke-WebRequest -Uri $wslUpdateUrl -OutFile $wslUpdatePackagePath
+Start-Process -Wait -FilePath msiexec.exe -ArgumentList "/i $wslUpdatePackagePath /quiet"
+Remove-Item -Path $wslUpdatePackagePath
+
+# Set WSL 2 as the default version
+wsl --set-default-version 2
+
+Write-Host "WSL 2 feature has been enabled and configured."
+}
+
+function appInstall {
 # Install Chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install apps (Chocolatey won't install it if it's already on the system)
 $apps = @('firefox', 'googlechrome', 'steam-client', 'discord',
 'notepadplusplus', 'vscode', 'spotify', 'greenshot', 'powertoys',
-'docker-desktop', '7zip')
+'docker-desktop', '7zip', 'runelite', 'adobereader', 'qgis', 'screentogif')
 
 choco install $apps -fy
+}
 
+function removeBloat {
 # Remove bloat
 $bloat_apps = @(
     "*Microsoft.3DBuilder*"
@@ -108,7 +136,10 @@ ForEach ($bloat in $bloat_apps){
 
     Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -like $bloat } |
      ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $_.PackageName }
+    }  
 }
 
-Write-Host "Done"
+wslInstall
+appInstall
+removeBloat
 
